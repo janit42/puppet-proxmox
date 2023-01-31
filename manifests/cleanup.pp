c280dbdd0c023fd87ad15db4d7a8e0d88bf586e3
Jan Sellmann <jan.sellmann@mpimet.mpg.de>
2023-01-31 17:08:44 +0100

    add safeguard: skip cleanup if node isn't running a proxmox pve kernel

--- manifests/cleanup.pp
+++ manifests/cleanup.pp
@@ -1,14 +1,16 @@
-# @summary This class removes the mainline linux kernel after a successful install
+# @summary This class removes the mainline linux kernel as soon as a proxmox pve kernel is running
 class proxmox::cleanup {
   $kernels = $facts['os']['release']['major'] ? {
     '10'    => 'linux-image-4.19*',
     '11'    => 'linux-image-5.10*',
     default => '',
   }
-  package { ['linux-image-amd64', $kernels, 'os-prober']:
-    ensure => 'absent',
-  }
-  ~>exec { '/usr/sbin/update-grub':
-    refreshonly => true,
+  if  /-pve$/ in $facts['kernelrelease'] {
+    package { ['linux-image-amd64', $kernels, 'os-prober']:
+      ensure => 'absent',
+    }
+    ~>exec { '/usr/sbin/update-grub':
+      refreshonly => true,
+    }
   }
 }
